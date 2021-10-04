8705cc93bd79d0673b141cd120ca89a9db4d18a6
Bhargav <bhargavkadali39@gmail.com>
2021-10-04 23:58:26 +0530

    Update my_captcha.py

--- my_captcha.py
+++ my_captcha.py
@@ -1,1 +1,6 @@
 
+from captcha.image import ImageCaptcha
+image = ImageCaptcha(width= 300,height = 100)
+captcha_text = 'my captcha'
+data = image.generate(captcha_text)
+image.write(captcha_text, 'CAPTCHA.png')
